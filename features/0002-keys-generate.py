31e045b27a6422ad5b5000e0410fc0da2864b01c
J Plasmeier <justplaz@amazon.com>
2023-09-19 16:35:08 -0700

    update spec, rev version, generate canonical manifest

--- features/0002-keys-generate.py
+++ features/0002-keys-generate.py
@@ -20,7 +20,7 @@ import sys
 AWS_REGION = "us-west-2"
 AWS_ACCOUNT_ID = "658956600833"
 
-VERSION = 3
+VERSION = 4
 AES_KEYS = (
     (128, b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15"),
     (
